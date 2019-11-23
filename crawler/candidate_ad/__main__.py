d6162068ef501fef913c54ac1301039d5f13336b
bangyuwen <bangyuwen@gmail.com>
2019-11-23 16:10:51 +0800

    AD stash

--- crawler/candidate_ad/__main__.py
+++ crawler/candidate_ad/__main__.py
@@ -1,7 +1,9 @@
+import json
+
 import numpy as np
 import pandas as pd
 
-from db import AD
+# from db import AD, mysql_db
 
 df = pd.read_csv("../data/manual/ad.csv")
 df.columns = [
@@ -13,7 +15,22 @@ df.columns = [
     "endDate",  # 結束日期
     "content",  # 廣告內容
 ]
-data = df.replace({np.nan: None}).to_dict(orient="records")
-AD.drop_table()
-AD.create_table()
-AD.insert_many(data).execute()
+
+data = df[df["content"].notnull()].replace({np.nan: None}).to_dict(orient="records")
+with open("../mobile/src/data/ad_static.json", "w") as fp:
+    json.dump(data, fp, ensure_ascii=False)
+
+# data = [
+#     {**i, i["content"]: bytes(i["content"] or "", "utf8")} for i in df.replace({np.nan: None}).to_dict(orient="records")
+# ]
+# AD.drop_table()
+# AD.create_table()
+# mysql_db.execute_sql(
+#     """ALTER TABLE
+#     ad
+#     CHANGE content content
+#     blob
+#     CHARACTER SET utf8mb4
+#     COLLATE utf8mb4_unicode_ci;"""
+# )
+# AD.insert_many(data).execute()
